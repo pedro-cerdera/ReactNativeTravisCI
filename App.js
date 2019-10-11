a7b510f02c2add818877497763e06c8b543aa69a
Pedro <pedro.cerdera@hotmail.com>
2019-10-11 00:46:33 -0300

    commit 51

--- App.js
+++ App.js
@@ -48,7 +48,7 @@ const App: () => React$Node = () => {
               </Text>
             </View>
             <View style={styles.sectionContainer}>
-              <Text style={styles.sectionTitle}>See Your Changes</Text>
+              <Text style={styles.sectionTitle}>See Your Changes.</Text>
               <Text style={styles.sectionDescription}>
                 <ReloadInstructions />
               </Text>
